9e9a724fadf4e35fc3a8c7b8cb84ead57ad35370
faustfizz <youssoufmbae2@gmail.com>
2023-03-24 14:29:40 +0300

    ---

--- core/I18n/Time.php
+++ core/I18n/Time.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the Footup 4 framework.
+ * This file is part of the CodeIgniter 4 framework.
  *
- * (c) Footup Foundation <admin@Footup.com>
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
