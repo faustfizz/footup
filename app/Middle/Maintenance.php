4aed2c423dd0a036affd050ca8a63158768bbd5c
faustfizz <youssoufmbae2@gmail.com>
2023-03-25 12:46:17 +0300

    remove Session param as required

--- app/Middle/Maintenance.php
+++ app/Middle/Maintenance.php
@@ -8,12 +8,12 @@ namespace App\Middle;
 
 use Footup\Http\Request;
 use Footup\Http\Response;
-use Footup\Http\Session;
+
 use Footup\Routing\Middle;
 
 class Maintenance extends Middle
 {
-    public function execute(Request $request, Response $response, Session $session)
+    public function execute(Request $request, Response $response)
     {
         return $response->die(503, "Site Web en Maintenance" ,"Ce site est en maintenance !");
     }
