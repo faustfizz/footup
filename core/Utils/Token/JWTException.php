4f0084076544c6839ea1143dd99fffebf9b468e2
faustfizz <youssoufmbae2@gmail.com>
2023-09-22 19:40:07 +0400

    new utils added

---
+++ core/Utils/Token/JWTException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * FOOTUP FRAMEWORK
+ * *************************
+ * A Rich Featured LightWeight PHP MVC Framework - Hard Coded by Faustfizz Yous
+ * 
+ * @package Footup\Utils\Token
+ * @version 0.1
+ * @link https://github.com/adhocore/php-jwt
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
+namespace Footup\Utils\Token;
+
+class JWTException extends \InvalidArgumentException
+{
+    // ;)
+}
